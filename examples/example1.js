6080dd8b893b7a56553ab8e911e7bb42e003517f
Oskar Nyberg <oskar@oskarnyberg.com>
2016-05-27 21:04:27 +0200

    Added an example.

---
+++ examples/example1.js
@@ -0,0 +1,30 @@
+const mpv = require('./../mpv').mpv;
+
+let state = 0;
+
+let player = new mpv(status => {
+    if (state === 0) {
+    } else if (status.playing && state === 1) {
+        player.pause();
+        console.log('Pausing');
+    } else if (!status.playing && state === 2) {
+        player.bigSeekForward();
+        console.log('Seeking forward');
+    } else if (!status.playing && state === 3) {
+        player.pause();
+        console.log('Resuming');
+    } else if (status.playing && state === 4) {
+        player.pause();
+        console.log('Pausing');
+    } else if (!status.playing && state === 5) {
+        player.stop();
+        console.log('Quitting');
+    } else {
+        console.error('Failed.')
+    }
+    state++;
+});
+
+player.limitStatusMessages(100);
+player.play('https://www.youtube.com/watch?v=rOOdfugvsIY');
+
