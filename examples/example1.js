cb5614b491498982e5034fa1ccd142762040d491
Oskar Nyberg <oskar@oskarnyberg.com>
2016-05-28 12:56:01 +0200

    Improved example.

--- examples/example1.js
+++ examples/example1.js
@@ -6,26 +6,24 @@ let player = new mpv(status => {
     if (state === 0) {
     } else if (status.playing && state === 1) {
         player.pause();
-        console.log('Pausing');
     } else if (!status.playing && state === 2) {
         player.bigSeekForward();
-        console.log('Seeking forward');
     } else if (!status.playing && state === 3) {
         player.pause();
-        console.log('Resuming');
     } else if (status.playing && state === 4) {
         player.pause();
-        console.log('Pausing');
     } else if (!status.playing && state === 5) {
         player.stop();
-        console.log('Quitting');
+    } else if (status.exit && state === 6) {
+        console.log('Success');
     } else {
-        console.error('Failed.')
+        console.error('Failed');
+        player.kill();
     }
+
     state++;
 });
 
-player.limitStatusMessages(100);
-player.play(["https://www.youtube.com/watch?v=rOOdfugvsIY", "--hwdec=no"
-        , "--ytdl-format=best"]);
+player.limitStatusMessages(10);
+player.play(["https://www.youtube.com/watch?v=rOOdfugvsIY", "--hwdec=no" , "--ytdl-format=best"]);
 
