a67b136f577641129981c1e1124ed508cd1c92a3
Oskar Nyberg <oskar@oskarnyberg.com>
2016-05-31 00:48:03 +0200

    Refactored status message handeling to it's own class.

--- mpv.js
+++ mpv.js
@@ -17,14 +17,11 @@ const pausedStrings = [
 
 class mpv {
     constructor(statusListener) {
-        this.statusListener = statusListener;
-        this.statusLimit = 1;
-        this.statusCounter = 0;
-        this.open = false;
+        this.dataHandler = new DataHandler(statusListener);
     }
 
     limitStatusMessages(mod) {
-        this.statusLimit = mod < 1 ? 1 : mod;
+        this.dataHandler.limit(mod);
     }
 
     play(first, second, third) {
@@ -49,53 +46,18 @@ class mpv {
     preparePlayer() {
         this.player.stdin.setEncoding('utf8');
         this.player.stderr.setEncoding('utf8');
-        this.player.stderr.on('data', this.handleData.bind(this));
-        this.player.stderr.on('close', this.closed.bind(this));
-    }
-
-    closed() {
-        if (typeof this.statusListener !== 'undefined' && this.open) {
-            this.open = false;
-            this.statusListener({
-                exit: true
-            });
-        }
-    }
-
-    handleData(data) {
-        this.open = true;
-        if (typeof this.statusListener !== 'undefined'
-                && this.statusCounter++ % this.statusLimit === 0) {
-            let status = this.parseData(data.toString());
-            this.statusListener(status);
-        }
-    }
-
-    parseData(data) {
-        let parts = data.split(' ');
-
-        let playing = pausedStrings.indexOf(parts[0]) === -1;
-        let buffering = parts[0] === pausedStrings[1];
-        if (!playing) {
-            parts.shift();
-        }
-
-        let percentage = parts[4].replace(/\(|\)|%/g, '')
-        let status = {
-            playing: playing,
-            buffering: buffering,
-            elapsed: parts[1],
-            total: parts[3],
-            progress: percentage / 100
-        }
-
-        return status;
+        this.player.stderr.on('data',
+                this.dataHandler.handleData.bind(this.dataHandler));
+        this.player.stderr.on('close', () => {
+            this.dataHandler.closed();
+            this.player == null;
+        });
     }
 
     // Should be done with this.player.stdin.write(key) which I can't manage to
     // get working.
     sendKey(key) {
-        if (this.open) {
+        if (this.player) {
             cp.exec('xdotool search --name " - mpv"', (_, result) => {
                 result = result.trim();
                 cp.exec('xdotool key --window ' + result + ' ' + key);
@@ -135,6 +97,66 @@ class mpv {
 
     displayStatus() {
         this.sendKey(keys.status);
+    }
+}
+
+class DataHandler {
+    constructor(listener) {
+        this.listener = listener;
+        this.statusLimit = 1;
+        this.statusCounter = 0;
+        this.open = false;
+    }
+
+    limit(mod) {
+        this.statusLimit = mod < 1 ? 1 : mod;
+    }
+
+    handleData(data) {
+        this.open = true;
+        if (typeof this.listener !== 'undefined'
+                && this.statusCounter++ % this.statusLimit === 0) {
+            let status = this.parseData(data.toString());
+            this.listener(status);
+        }
+    }
+
+    closed() {
+        if (typeof this.listener !== 'undefined' && this.open) {
+            this.open = false;
+            this.listener({
+                exit: true
+            });
+        }
+    }
+
+    parseData(data) {
+        let parts = data.split(' ');
+
+        let playing = pausedStrings.indexOf(parts[0]) === -1;
+        let buffering = parts[0] === pausedStrings[1];
+        if (!playing) {
+            parts.shift();
+        }
+
+        let percentage = parts[4].replace(/\(|\)|%/g, '')
+        let status = {
+            playing: playing,
+            buffering: buffering,
+            elapsed: this.parseTime(parts[1]),
+            total: this.parseTime(parts[3]),
+            elapsedStr: parts[1],
+            totalStr: parts[3],
+            progress: percentage / 100
+        }
+
+        return status;
+    }
+
+    parseTime(time) {
+        var arr = time.split(':');
+        var seconds = (+arr[0]) * 60 * 60 + (+arr[1]) * 60 + (+arr[2]);
+        return seconds;
     }
 }
 
