78cd8b94cf4e9cb57237db111d7fb0ec6382a25f
Oskar Nyberg <oskar@oskarnyberg.com>
2016-06-11 22:47:32 +0200

    Extracted command list to its own file.

--- mpv.js
+++ mpv.js
@@ -1,23 +1,6 @@
 const cp = require('child_process'),
-      FIFO = require('fifo-js');
-
-const commands = {
-    togglePause: 'cycle pause',
-    stop: 'stop',
-    status: 'show-progress',
-    seekBackward: 'seek -5',
-    seekForward: 'seek +5',
-    bigSeekBackward: 'seek -30',
-    bigSeekForward: 'seek +30',
-    increaseSpeed: 'multiply speed 1.1',
-    decreaseSpeed: 'multiply speed 0.9',
-    resetSpeed: 'set speed 1',
-    decreaseVolume: 'add volume -5',
-    increaseVolume: 'add volume 5',
-    mute: 'cycle mute',
-    toggleFullscreen: 'cycle fullscreen',
-    toggleSubtitle: 'cycle sub'
-};
+      FIFO = require('fifo-js'),
+      commands = require('./commands');
 
 const pausedStrings = [
     "(Paused)",
